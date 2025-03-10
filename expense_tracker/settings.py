8a976009561923f60f06c4f7e550fb2f02c3acf3
Akinwale Feranmi Philip <feranmiphileo@gmail.com>
2025-03-10 14:10:20 +0100

    Updated settings.py

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +77,13 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
